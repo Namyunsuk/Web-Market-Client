666fa305df76939e9a56981420024a88b091cdd6
Namyunsuk <yunsuk0328@naver.com>
2022-07-27 00:39:42 +0900

    슬라이딩 배너 구현, 상품 업로드, 결제하기

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,11 +1,51 @@
-import { Form, Divider, Input, InputNumber, Button } from "antd";
+import {
+  Form,
+  Divider,
+  Input,
+  InputNumber,
+  Button,
+  Upload,
+  message,
+} from "antd";
+import { useState } from "react";
 import "./index.css";
+import { API_URL } from "../config/constants.js";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function UploadPage() {
+  const [imageUrl, setImageUrl] = useState(null);
+  const history = useHistory();
   const onSubmit = (values) => {
     console.log(values); //values의 값들은 name속성이 key가 되고 입력값이 value가 된 형태로 들어간다.
+    axios
+      .post(`${API_URL}/products`, {
+        name: values.name,
+        description: values.description,
+        seller: values.seller,
+        price: parseInt(values.price),
+        imageUrl: imageUrl,
+      })
+      .then((result) => {
+        console.log(result);
+        history.replace("/");
+      })
+      .catch((err) => {
+        console.error(err);
+        message.error(`에러가 발생했습니다. ${err.message}`);
+      });
   };
   // Form은 Form.Itemㅇ로부터 데이터를 관리한다.
+  const onChangeImage = (info) => {
+    if (info.file.status === "uploading") {
+      return;
+    }
+    if (info.file.status === "done") {
+      const response = info.file.response;
+      const imageUrl = response.imageUrl;
+      setImageUrl(imageUrl);
+    }
+  };
   return (
     <div id="upload-container">
       <Form name="상품 업로드" onFinish={onSubmit}>
@@ -13,10 +53,22 @@ function UploadPage() {
           name="upload"
           label={<div className="upload-label">상품 사진</div>}
         >
-          <div id="upload-img-placeholder">
-            <img src="/images/icons/camera.png" />
-            <span>이미지를 업로드해주세요.</span>
-          </div>
+          <Upload
+            name="image"
+            action={`${API_URL}/image`}
+            listType="picture"
+            showUploadList={false}
+            onChange={onChangeImage}
+          >
+            {imageUrl ? (
+              <img id="upload-img" src={`${API_URL}/${imageUrl}`} />
+            ) : (
+              <div id="upload-img-placeholder">
+                <img src="/images/icons/camera.png" />
+                <span>이미지를 업로드해주세요.</span>
+              </div>
+            )}
+          </Upload>
         </Form.Item>
         <Divider />
         <Form.Item

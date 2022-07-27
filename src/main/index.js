bbe4d9ce16b882296d864d8e31fc4ef64ed3546f
Namyunsuk <yunsuk0328@naver.com>
2022-07-27 11:40:11 +0900

    결제하기, 반응형 웹 구현

--- src/main/index.js
+++ src/main/index.js
@@ -52,6 +52,7 @@ function MainPage() {
         {products.map(function (product, index) {
           return (
             <div className="product-card">
+              {product.soldout === 1 && <div className="product-blur" />}
               <Link className="product-link" to={`/products/${product.id}`}>
                 <div>
                   <img
--- src/product/index.js
+++ src/product/index.js
@@ -4,11 +4,12 @@ import { useEffect, useState } from "react";
 import "./index.css";
 import { API_URL } from "../config/constants.js";
 import dayjs from "dayjs";
+import { Button, message } from "antd";
 
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-  useEffect(function () {
+  const getProduct = () => {
     axios
       .get(`${API_URL}/products/${id}`)
       .then(function (result) {
@@ -17,11 +18,26 @@ function ProductPage() {
       .catch(function (error) {
         console.error("에러 발생: ", error);
       });
+  };
+  useEffect(function () {
+    getProduct();
   }, []);
 
   if (product === null) {
     return <h1>상품 정보를 받고 있습니다...</h1>;
   }
+
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("구매가 완료되었습니다.");
+        getProduct();
+      })
+      .catch((err) => {
+        message.error(`Error. ${err.message}`);
+      });
+  };
   return (
     <div>
       <div id="image-box">
@@ -37,6 +53,16 @@ function ProductPage() {
         <div id="createdAt">
           {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
         </div>
+        <Button
+          id="purchase-button"
+          size="large"
+          type="primary"
+          danger={true}
+          onClick={onClickPurchase}
+          disabled={product.soldout === 1}
+        >
+          구매하기
+        </Button>
         <pre id="description">{product.description}</pre>
       </div>
     </div>
